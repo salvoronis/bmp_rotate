09d1899b40ad34167c2da5e14e64f6226e610c5c
Яремко Роман <salvoronis@gmail.com>
2020-10-18 17:05:46 +0300

    finally

--- src/main.c
+++ src/main.c
@@ -22,8 +22,13 @@ struct __attribute__((packed)) bmp_header {
 	uint32_t biClrImportant;
 };
 
-struct pixel {
+struct __attribute__((packed)) pixel {
 	unsigned char b, g, r;
+};
+
+struct __attribute__((packed)) image {
+	uint32_t width,height;
+	struct pixel *pixels;
 };
 
 enum Error{
@@ -33,49 +38,89 @@ enum Error{
 	BITCOUNT_ERR
 };
 
-//void print_header(struct bmp_header*);
+void print_header(struct bmp_header*);
+struct image *parse_image(FILE *image,uint32_t width, uint32_t height);
+enum Error read_header(FILE *image, struct bmp_header *header);
+struct image *rotate_image(struct image *image);
+struct bmp_header rotate_header(struct bmp_header header);
+void load_image(struct image *img, struct bmp_header *header, FILE *image);
 
 int main(void){
-	FILE *image = fopen("./res/strelka.bmp", "rb");
-	if (image == NULL){
-		perror("file error");
-		exit(FILE_ERR);
-	}
+	FILE *image = fopen("./res/anime.bmp", "rb");
 	struct bmp_header *header = malloc(sizeof(struct bmp_header));
-	fread(header, sizeof(struct bmp_header), 1, image);
-	if (header->bfType != MAGIC) {
-		perror("File format error");
-		exit(TYPE_ERR);
-	} else if (header->biBitCount != 24) {
-		perror("Bit count is not 24");
-		exit(BITCOUNT_ERR);
+	if (read_header(image, header) != OK){
+		exit(1);
 	}
-
-	struct pixel rotate[header->biHeight][header->biWidth];
-
+	
 	fseek(image, header->bOffbits, SEEK_SET);
 
-	//print_header(header);
+	struct image *origin = parse_image(image, header->biWidth, header->biHeight);
+
+	struct image *rotated = rotate_image(origin);
+	struct bmp_header rotated_header = rotate_header(*header);
+
+	FILE *new_img = fopen("./res/newanime.bmp", "wb");
 	
-	for (int i = header->biHeight-1; i>=0; i--) {
-		for (int j = 0; j<header->biWidth; j++) {
-			fread(&rotate[i][j],sizeof(struct pixel),1,image);
-		}
-	}
-	fclose(image);
-	FILE *rotated = fopen("./res/strelkanew.bmp", "wb");
-	fwrite(header, sizeof(struct bmp_header), 1, rotated);
-	//print_header(header);
-	for (int i = header->biWidth-1; i>=0; i--) {
-		for (int j = header->biHeight-1; j>=0; j--) {
-			fwrite(&rotate[j][i], sizeof(struct pixel), 1, rotated);
-		}
-	}
+	/*fwrite(&rotated_header, sizeof(struct bmp_header), 1, new_img);
+	fwrite(rotated->pixels, sizeof(struct pixel), rotated->height*rotated->width, new_img);*/
+	load_image(rotated, &rotated_header, new_img);
 
 	return 0;
 }
 
-/*void print_header(struct bmp_header *header){
+void load_image(struct image *img, struct bmp_header *header, FILE *image){
+	fwrite(header, sizeof(struct bmp_header), 1, image);
+	fwrite(img->pixels, sizeof(struct pixel), img->height*img->width, image);
+}
+
+enum Error read_header(FILE *image, struct bmp_header *header){
+	if (image == NULL){
+		return FILE_ERR;
+	}
+
+	fread(header, sizeof(struct bmp_header), 1, image);
+	
+	if (header->bfType != MAGIC) {
+		return TYPE_ERR;
+	} else if (header->biBitCount != 24) {
+		return BITCOUNT_ERR;
+	}
+	return OK;
+}
+
+struct image *rotate_image(struct image *origin){
+	struct image *new_image = malloc(sizeof(struct image));
+	new_image->height = origin->width;
+	new_image->width = origin->height;
+	new_image->pixels = (struct pixel*)malloc(new_image->width*sizeof(struct pixel)*new_image->height);
+
+	for (uint32_t i = 0; i<new_image->height; i++) {
+		for (uint32_t j = 0; j<new_image->width; j++) {
+			*(new_image->pixels + i*new_image->width + j) = *(origin->pixels + j*origin->width + (origin->width-1-i));
+		}
+	}
+	return new_image;
+}
+
+struct bmp_header rotate_header(struct bmp_header origin){
+	uint32_t tmp = origin.biHeight;
+	origin.biHeight = origin.biWidth;
+	origin.biWidth = tmp;
+	return origin;
+}
+
+struct image *parse_image(FILE *image,uint32_t width, uint32_t height){
+	struct image *img = malloc(sizeof(struct image));
+	//int padding = (4- (width*sizeof(struct pixel)) % 4) % 4;
+	img->height = height;
+	img->width = width;
+	img->pixels = (struct pixel*)malloc(img->width*sizeof(struct pixel)*img->height);
+
+	fread(img->pixels, sizeof(struct pixel), img->height*img->width, image);
+	return img;
+}
+
+void print_header(struct bmp_header *header){
 	printf("type->%X fileSize->%X Reserver->%X offbits->%X Size->%u width->%u height->%u planes->%X bitcount->%u compression->%X sizeimg->%u xpels->%X ypels->%X clrused->%X importatn->%X\n",
 			header->bfType,
 			header->bfileSize,
@@ -92,4 +137,4 @@ int main(void){
 			header->biYPelsPerMeter,
 			header->biClrUsed,
 			header->biClrImportant);
-}*/
+}
