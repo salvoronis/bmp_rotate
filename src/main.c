21fb75347ca6b05db210772b333c5b111d8e3b61
Яремко Роман <salvoronis@gmail.com>
2020-10-18 17:54:07 +0300

    const(

--- src/main.c
+++ src/main.c
@@ -1,49 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "./main.h"
 
 #define MAGIC 0x4d42
-
-struct __attribute__((packed)) bmp_header {
-	uint16_t bfType;
-	uint32_t bfileSize;
-	uint32_t bfReserved;
-	uint32_t bOffbits;
-	uint32_t biSize;
-	uint32_t biWidth;
-	uint32_t biHeight;
-	uint16_t biPlanes;
-	uint16_t biBitCount;
-	uint32_t biCompression;
-	uint32_t biSizeImage;
-	uint32_t biXPelsPerMeter;
-	uint32_t biYPelsPerMeter;
-	uint32_t biClrUsed;
-	uint32_t biClrImportant;
-};
-
-struct __attribute__((packed)) pixel {
-	unsigned char b, g, r;
-};
-
-struct __attribute__((packed)) image {
-	uint32_t width,height;
-	struct pixel *pixels;
-};
-
-enum Error{
-	OK,
-	FILE_ERR,
-	TYPE_ERR,
-	BITCOUNT_ERR
-};
-
-void print_header(struct bmp_header*);
-struct image *parse_image(FILE *image,uint32_t width, uint32_t height);
-enum Error read_header(FILE *image, struct bmp_header *header);
-struct image *rotate_image(struct image *image);
-struct bmp_header rotate_header(struct bmp_header header);
-void load_image(struct image *img, struct bmp_header *header, FILE *image);
 
 int main(void){
 	FILE *image = fopen("./res/anime.bmp", "rb");
@@ -55,15 +15,15 @@ int main(void){
 	fseek(image, header->bOffbits, SEEK_SET);
 
 	struct image *origin = parse_image(image, header->biWidth, header->biHeight);
+	fclose(image);
 
 	struct image *rotated = rotate_image(origin);
 	struct bmp_header rotated_header = rotate_header(*header);
 
 	FILE *new_img = fopen("./res/newanime.bmp", "wb");
-	
-	/*fwrite(&rotated_header, sizeof(struct bmp_header), 1, new_img);
-	fwrite(rotated->pixels, sizeof(struct pixel), rotated->height*rotated->width, new_img);*/
+
 	load_image(rotated, &rotated_header, new_img);
+	fclose(new_img);
 
 	return 0;
 }
@@ -111,30 +71,10 @@ struct bmp_header rotate_header(struct bmp_header origin){
 
 struct image *parse_image(FILE *image,uint32_t width, uint32_t height){
 	struct image *img = malloc(sizeof(struct image));
-	//int padding = (4- (width*sizeof(struct pixel)) % 4) % 4;
 	img->height = height;
 	img->width = width;
 	img->pixels = (struct pixel*)malloc(img->width*sizeof(struct pixel)*img->height);
 
 	fread(img->pixels, sizeof(struct pixel), img->height*img->width, image);
 	return img;
-}
-
-void print_header(struct bmp_header *header){
-	printf("type->%X fileSize->%X Reserver->%X offbits->%X Size->%u width->%u height->%u planes->%X bitcount->%u compression->%X sizeimg->%u xpels->%X ypels->%X clrused->%X importatn->%X\n",
-			header->bfType,
-			header->bfileSize,
-			header->bfReserved,
-			header->bOffbits,
-			header->biSize,
-			header->biWidth,
-			header->biHeight,
-			header->biPlanes,
-			header->biBitCount,
-			header->biCompression,
-			header->biSizeImage,
-			header->biXPelsPerMeter,
-			header->biYPelsPerMeter,
-			header->biClrUsed,
-			header->biClrImportant);
 }
