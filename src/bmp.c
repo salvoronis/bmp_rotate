e56a198033e40b950b75080cd5fed2b0c73a0e42
Яремко Роман <salvoronis@gmail.com>
2020-11-06 17:43:46 +0300

    structure

--- src/bmp.c
+++ src/bmp.c
@@ -1,28 +1,7 @@
-#include <stdlib.h>
 #include "bmp.h"
-
-int main(int argc, char * argv[]){
-	FILE *image = fopen(argv[1], "rb");
-	struct bmp_header *header = malloc(sizeof(struct bmp_header));
-	if (read_header(image, header) != OK){
-		exit(1);
-	}
-	
-	fseek(image, header->bOffbits, SEEK_SET);
-
-	struct image *origin = parse_image(image, header->biWidth, header->biHeight);
-	fclose(image);
-
-	struct image *rotated = rotate_image(origin);
-	struct bmp_header rotated_header = rotate_header(*header);
-
-	FILE *new_img = fopen("./res/result.bmp", "wb");
-
-	load_image(rotated, &rotated_header, new_img);
-	fclose(new_img);
-
-	return 0;
-}
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 void load_image(struct image * const img, struct bmp_header * const header, FILE * const image){
 	fwrite(header, sizeof(struct bmp_header), 1, image);
